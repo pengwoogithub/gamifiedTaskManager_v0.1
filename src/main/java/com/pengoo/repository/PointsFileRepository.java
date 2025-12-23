49e7bd7fb2f9e2325e1d154432fd62d69cacee07
pengwoogithub <148692807+pengwoogithub@users.noreply.github.com>
2025-12-23 10:52:04 +0800

    dis da new code

--- src/main/java/com/pengoo/repository/PointsFileRepository.java
+++ src/main/java/com/pengoo/repository/PointsFileRepository.java
@@ -1,6 +1,20 @@
 package com.pengoo.repository;
 
+import java.io.IOException;
+import java.nio.file.*;
+
 public class PointsFileRepository {
 
+    private int totalPoints;
+    private Path pointsPath = FileSystems.getDefault().getPath("data","points.txt");
+
+
+    public PointsFileRepository(){
+
+    }
+
+    public void loadPoints(){
+
+    }
 
 }
